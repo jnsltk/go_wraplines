80241953a4b9bc9d7bc6a6a9f717faa3db7f539a
jnsltk <janos.litkei@gmail.com>
2024-12-08 21:34:57 +0100

    Implement wrapping and "smart mode"

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func loadFile(path string) string {
 func parseArgs() (*string, *int, *bool) {
 	filePathPtr := flag.String("f", "none", "Input file path")
 	wrapPtr := flag.Int("w", 80, "Wrap text at column N")
-	isSmartPtr := flag.Bool("s", true, "Sets if the tool should recognise words")
+	isSmartPtr := flag.Bool("smart", true, "Sets if the tool should recognise words")
 
 	flag.Parse()
 	if *filePathPtr == "none" {
@@ -38,10 +38,29 @@ func parseArgs() (*string, *int, *bool) {
 func wrapText(text string, wrap int, isSmart bool) string {
 	// Remove all existing end line characters
 	text = strings.Replace(text, "\n", " ", -1)
-	for i := 0; i < len(text); i++ {
-		if i%wrap == 0 {
-			text = text[:i] + "\n" + text[i:]
+	if isSmart {
+		for i := 0; i < len(text); i++ {
+			if i%wrap == 0 && i > 0 {
+				j := i
+				for text[j] != ' ' {
+					j--
+				}
+				text = replaceAtIndex(text, '\n', j)
+			}
+		}
+	} else {
+		for i := 0; i < len(text); i++ {
+			if i%wrap == 0 && i > 0 {
+				text = text[:i] + "\n" + text[i:]
+			}
 		}
 	}
+
 	return text
+}
+
+func replaceAtIndex(input string, char rune, i int) string {
+	out := []rune(input)
+	out[i] = char
+	return string(out)
 }
