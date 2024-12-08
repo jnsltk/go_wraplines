f2a13c13bc6b507557cee69f98dda693ba53fa07
jnsltk <janos.litkei@gmail.com>
2024-12-08 19:46:54 +0100

    Parse arguments and load file

--- main.go
+++ main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
-	"path/filepath"
+	"strconv"
+	"strings"
 )
 
+// ════════════════════════════════════ Functions ═════════════════════════════════════
 func main() {
-	executablePath, err := os.Executable()
+	filePath, wrap, isSmart := parseArgs()
+	text := loadFile(*filePath)
+	fmt.Println(wrapText(text, *wrap, *isSmart))
+}
+
+func loadFile(path string) string {
+	dat, err := os.ReadFile(path)
 	if err != nil {
-		log.Fatalf("Error getting executable path: %v", err)
+		log.Fatalf("Error reading file: %v", err)
 	}
-	executableDir := filepath.Dir(executablePath)
-	fmt.Printf(executableDir)
+	return string(dat)
+}
+
+func parseArgs() (*string, *int, *bool) {
+	filePathPtr := flag.String("f", "none", "Input file path")
+	wrapPtr := flag.Int("w", 80, "Wrap text at column N")
+	isSmartPtr := flag.Bool("s", true, "Sets if the tool should recognise words")
+
+	flag.Parse()
+	if *filePathPtr == "none" {
+		log.Fatal("Please provide a file path")
+	}
+	return filePathPtr, wrapPtr, isSmartPtr
+}
+
+func wrapText(text string, wrap int, isSmart bool) string {
+	text = strings.Replace(text, "\n", " ", -1)
+	wrapStr := strconv.Itoa(wrap)
+	return text + wrapStr
 }
